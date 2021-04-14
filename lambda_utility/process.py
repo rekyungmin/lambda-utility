e4e599ca4474124aeb134cd7222949befad1cd74
Kyungmin Lee <rekyungmin@gmail.com>
2021-04-14 18:08:34 +0900

    :sparkles: add optionize function

--- lambda_utility/process.py
+++ lambda_utility/process.py
@@ -2,13 +2,15 @@ from __future__ import annotations
 
 __all__ = (
     "ProcessError",
+    "optionize",
     "run_command",
 )
 
 import asyncio
+import enum
 import sys
 import logging
-from typing import Optional
+from typing import Optional, Union
 
 logger = logging.getLogger(__file__)
 
@@ -27,6 +29,31 @@ class ProcessError(Exception):
         self.stdin = stdin
         self.stdout = stdout
         self.stderr = stderr
+
+
+def optionize(*params: Union[str, tuple[str, Union[str, float, bool]]]) -> list[str]:
+    """
+    :example:
+        >>> optionize("-y", ("-pix_fmt", "yuv420p"), ("-framerate", 29.97), ("-condition", True), ("-condition2", False), ("-condition3", None))
+        ['-y', '-pix_fmt', 'yuv420p', '-framerate', '29.97', '-condition']
+    """
+    options = []
+    for p in params:
+        if isinstance(p, str):
+            options.append(p)
+        elif isinstance(p, (int, float)):
+            options.append(str(p))
+        elif isinstance(p, enum.Enum):
+            options.append(str(p.value))
+        else:
+            if len(p) == 2 and (p[1] is None or p[1] is True or p[1] is False):
+                if p[1] is True:
+                    options.append(p[0])
+                else:
+                    continue
+            else:
+                options.extend(str(value) for value in p)
+    return options
 
 
 async def run_command(command: str, *params: str) -> tuple[bytes, bytes]:
